789e23f408d2ff43744ecf86cbe2c9ccbee2ad77
curiocortex <152873166+curiocortex@users.noreply.github.com>
2024-03-05 18:11:57 +0330

    Update base.py

--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -255,7 +255,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # https://github.com/pennersr/django-allauth
-ACCOUNT_AUTHENTICATION_METHOD = 'username' 
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email' 
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -705,6 +705,11 @@ SMOKESCREEN_ENABLED = True
 # But it won't work without additional configuration, so we set it to None
 # to check configuration and throw ImproperlyConfigured if thats a case
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ‘smtp.gmail.com’
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ‘smartixai0@gmail.com’
+EMAIL_HOST_PASSWORD = ‘Sm4420674237’
 
 ONE_RUNNING_JOB_IN_QUEUE_PER_USER = to_bool(os.getenv('ONE_RUNNING_JOB_IN_QUEUE_PER_USER', False))
 
